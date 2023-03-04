3a86244956e2a6c3623cb2c2f368e495fc89efd2
flow1980 <flow1980@gmx.net>
2023-03-04 16:42:29 +0100

    Width of the navigation menus modified.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,5 @@
 /*
 const navHexWidth     = document.getElementById("nav-hex-tools").style.width;
-const navSrecordWidth = document.getElementById("nav-s-record-tools").style.width;
-const navOtherWidth   = document.getElementById("nav-other-tools").style.width;
-
-const maxNavWidth = Math.max(navHexWidth, navSrecordWidth, navOtherWidth);
-  
-document.getElementById("nav-hex-tools").style.width = maxNavWidth;
-document.getElementById("nav-s-record-tools").style.width = maxNavWidth;
-document.getElementById("nav-other-tools").style.width = maxNavWidth;
+document.getElementById("nav-hex-tools").style.width = navHexWidth;
 */
   
