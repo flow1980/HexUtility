eb24b2450e345cbc6a761634ff49c564648feea9
Lengert Florian (BEG-CD/ECS3) <Florian.Lengert@de.bosch.com>
2023-03-14 16:52:12 +0100

    Hex2Bin tool development.

--- app-hex2bin/hex2bin.js
+++ app-hex2bin/hex2bin.js
@@ -1,23 +1,39 @@
 import { hexFilePickerOpts, binFilePickerOpts } from "../scripts/helper-functions.js";
 
 
+let fileList;
 let hexfileContent;
+let binFile;
 
 const inputElementHexfile = document.getElementById("inputHexfile");
 inputElementHexfile.addEventListener("change", readHexfile, false);
 
 async function readHexfile() {
-   const [fileList] = this.files;
+   [fileList] = this.files;
    
    if (fileList) {
       const reader = new FileReader();
       reader.addEventListener("load", () => {
          hexfileContent = reader.result;
-         console.log(hexfileContent);
        });
        reader.readAsText(fileList);
    }
 }
 
+const btnElement = document.getElementById("hex2bin-downloadBinfile");
+btnElement.addEventListener("click", downloadBinfile);
 
+function downloadBinfile() {
+   binFile = new Blob([hexfileContent]);
+   const link = document.createElement('a');
+   link.style.display = 'none';
+   const url = URL.createObjectURL(binFile);
+   link.href = url;
+   link.download = fileList.name.replace('.hex', '.bin');
 
+   document.body.appendChild(link); /* It needs to be added to the DOM so it can be clicked. */
+   link.click();
+   document.body.removeChild(link);
+ 
+   window.URL.revokeObjectURL(url);
+}
