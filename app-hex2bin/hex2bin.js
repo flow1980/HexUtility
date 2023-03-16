61162c461c7450898e4d313bf6b4bc10dfe25462
flow1980 <flow1980@gmx.net>
2023-03-16 21:55:20 +0100

    Hex2Bin tool development.

--- app-hex2bin/hex2bin.js
+++ app-hex2bin/hex2bin.js
@@ -1,4 +1,4 @@
-import { hexFilePickerOpts, binFilePickerOpts } from "../scripts/helper-functions.js";
+import { hexFilePickerOpts, binFilePickerOpts, hexFilePattern, isRawHexFile, convertHexStringToByteArray, convertByteArrayToHexstring } from "../scripts/helper-functions.js";
 
 
 let hexFile;
@@ -6,32 +6,128 @@ let hexFileContent;
 let binFile;
 
 const inputHexfile = document.getElementById("hex2bin-inputHexfile");
-inputHexfile.addEventListener("change", readHexfile, false);
+inputHexfile.addEventListener("change", selectHexfile);
 
-async function readHexfile() {
+async function selectHexfile()
+{
    hexFile = this.files[0];
-   
-   if (hexFile) {
+
+   if (hexFile)
+   {
       buttonStartConversion.disabled = false;
    }
 }
 
 
 const buttonStartConversion = document.getElementById("hex2bin-startConversion");
-buttonStartConversion.addEventListener("click", Hex2Bin);
+buttonStartConversion.addEventListener("click", startHex2Bin);
 
-function Hex2Bin () {
+function startHex2Bin()
+{
    const reader = new FileReader();
    reader.addEventListener("load", () => {
       hexFileContent = reader.result;
-      downloadBinfile();
+       Hex2Bin(hexFileContent);
    });
    reader.readAsText(hexFile);
 }
 
 
-function downloadBinfile() {
-   binFile = new Blob([hexFileContent]);
+function Hex2Bin(hexFileContent) {
+   /*********************************************************************************/
+   /*** Variables declarations                                                      */
+   /*********************************************************************************/
+   let result = 0;
+   let addr = 0;
+   let absaddr = 0;
+   let currAbsaddr = 0;
+   let offset = 0;
+   let startIndex = 0;
+   let endIndex = 0;
+
+   let hexFileLines = hexFileContent.split("\n");
+   let line = "";
+   let lineAsBytes = [];
+   let binFileContent = [];
+
+   let matches = [];
+
+   let recordType = "";
+
+   if (isRawHexFile(hexFileContent))
+   {
+      for (let i = 0; (i < (hexFileLines.length)) && (hexFileLines[i].length > 0); i++)
+      {
+         let lineAsBytesObject = convertHexStringToByteArray(hexFileLines[i]);
+
+         if (false == lineAsBytesObject.result)
+         {
+            result = 1;
+         }
+         else
+         {
+            binFileContent.push(...lineAsBytesObject.byteArray);
+         }
+      }
+   }
+   else 
+   {
+      for (let i = 0; (i < (hexFileLines.length)) && (0 === result) && (hexFileLines[i].length > 0); i++)
+      {
+         /* Match the regular expression pattern against a text string. */
+         matches = hexFilePattern.exec(hexFileLines[i]);
+         if (null !== matches)
+         {
+            /* 
+               Filter for data records (record type '00') according to Intel HEX format
+               The first element of the GroupCollection object (match.Groups[0]) contains a string
+               that matches the entire regular expression pattern.
+               Each subsequent element represents a captured group, if the regular expression includes
+               capturing groups.
+            */
+            recordType = matches[3];
+            /* Data Record */
+            if ("00" === recordType)
+            {
+               let lineAsBytesObject = convertHexStringToByteArray(matches[4]);
+
+               if (false == lineAsBytesObject.result)
+               {
+                  result = 1;
+               }
+               else
+               {
+                  binFileContent.push(...lineAsBytesObject.byteArray);
+               }
+            }
+         }
+         else
+         {
+            result = 1;
+            
+         }
+     }
+   }
+
+   if (0 != result)
+   {
+      alert("Error: HEX file is corrupt !!!");
+   }
+   else
+   {
+      downloadBinfile(binFileContent);
+   }
+
+   return result;
+}
+
+
+
+function downloadBinfile(binFileContent)
+{
+   let byteArray = new Uint8Array(binFileContent);
+   binFile = new Blob([byteArray], { type: "application/octet-stream" });
+   
    const link = document.createElement('a');
    link.style.display = 'none';
    const url = URL.createObjectURL(binFile);
@@ -41,6 +137,6 @@ function downloadBinfile() {
    document.body.appendChild(link); /* It needs to be added to the DOM so it can be clicked. */
    link.click();
    document.body.removeChild(link);
- 
+
    window.URL.revokeObjectURL(url);
 }
--- scripts/helper-functions.js
+++ scripts/helper-functions.js
@@ -1,11 +1,11 @@
 const hexFilePickerOpts = {
    types: [
-     {
-       description: "HEX file",
-       accept: {
-         "text/*": [".hex"]
-       }
-     },
+      {
+         description: "HEX file",
+         accept: {
+            "text/*": [".hex"]
+         }
+      },
    ],
    excludeAcceptAllOption: true,
    multiple: false
@@ -13,13 +13,105 @@ const hexFilePickerOpts = {
 
 const binFilePickerOpts = {
    types: [
-     {
-       description: 'BIN file',
-       accept: '.bin'
-     },
+      {
+         description: 'BIN file',
+         accept: '.bin'
+      },
    ],
    excludeAcceptAllOption: true,
    multiple: false
 };
 
-export { hexFilePickerOpts, binFilePickerOpts };
+const hexFilePattern = /^:(\w{2})(\w{4})(\w{2})(\w*)(\w{2})/;
+/*
+   ^  : The comparison must start at the beginning of a string or line.
+   \w : Matches any word character.
+   {2}: Matches the previous element exactly 2 times.
+   \w*: Matches any word character zero or more times.
+   () : Captures the matched subexpression and assigns it a zero-based ordinal number.
+*/
+
+
+/* 
+ * --------------------------------------------------------------------
+ * Checks if the HEX file content is in RAW hex format or not (Intel HEX format).
+ *    @param   HEX file content (string)
+ *    @return  Result (true: Hex digit, false: No Hex Digit)
+ * --------------------------------------------------------------------
+ */
+function isRawHexFile(hexFileContent)
+{
+   let retval = false;
+
+   if (hexFileContent.includes(":"))
+   {
+      retval = false;
+   }
+   else
+   {
+      retval = true;
+   }
+ 
+   return retval;
+}
+
+
+/* 
+ * --------------------------------------------------------------------
+ * Converts a hexadecimal string to a byte array.
+ *    @param   Hexadecimal string
+ *    @return  Conversion result (true: successful, false: not successful) AND
+ *             byte array
+ * --------------------------------------------------------------------
+ */
+function convertHexStringToByteArray(hexString)
+{
+   let result = true;
+   let byteArray = [];
+
+   if (hexString.length % 2 != 0)
+   {
+      result = false;
+      alert("The binary key cannot have an odd number of digits: $(hexString)");
+   }
+ 
+   for (let c = 0; (c < hexString.length) && (true === result); c += 2)
+   {
+      byteArray.push(parseInt(hexString.substring(c, c + 2), 16));
+      /*
+         push:      [array]  adds one element to the end of an array.
+         parseInt:  [number] parses a string argument and returns an integer of the specified radix or base.         
+         substring: [string] returns the part of the string from the start index up to and excluding the end index.  
+      */
+   }
+ 
+   return {
+      "result": result,
+      "byteArray": byteArray
+   };
+}
+
+
+/* 
+ * --------------------------------------------------------------------
+ * Converts a byte array to a hexadecimal string.
+ *    @param   Byte array
+ *    @return  hexadecimal string
+ * --------------------------------------------------------------------
+ */
+function convertByteArrayToHexstring (byteArray)
+{
+   let hex = [];
+
+   for (let i = 0; i < byteArray.length; i++)
+   {
+      let current = byteArray[i] < 0 ? byteArray[i] + 256 : byteArray[i];
+      hex.push((current >>> 4).toString(16)); /* add the most  significant hex digit of one byte to the hex array */
+      hex.push((current & 0xF).toString(16)); /* add the least significant hex digit to one byte to the hex array */
+   }
+
+   return hex.join(""); /* returns a new string by concatenating all of the elements in an array, with the ""-separator */
+}
+
+
+export { hexFilePickerOpts, binFilePickerOpts, hexFilePattern, isRawHexFile, convertHexStringToByteArray, convertByteArrayToHexstring };
