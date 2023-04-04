acd1e7fb1bf7c3b6ee229c383982735085753656
Lengert Florian (BEG-CD/ECS3) <Florian.Lengert@de.bosch.com>
2023-04-04 16:53:43 +0200

    [OTA Master Configurator] Tool development.

--- app-ota-master-configurator/ota-master-configurator.js
+++ app-ota-master-configurator/ota-master-configurator.js
@@ -2,3 +2,5 @@ import { updateManifestJsoneditor } from "./update-manifest-jsoneditor.js";
 
 const element = document.getElementById("json-editor");
 const editor = new JSONEditor(element, updateManifestJsoneditor);
+
+console.log(editor);
